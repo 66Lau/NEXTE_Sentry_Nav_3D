15815968c4590d7436d8b0c2fe6219e95a579855
66Lau <liuhang20020917@gmail.com>
2023-12-24 16:29:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Navigation system in a 3D environment(for real robot "sentry")
+
+## Overview
+
+
+## In&out put
+
+### cmu base planner
+
+input :
+  - sensor_msgs::PointCloud2: `/registered_scan` 
+  - nav_msgs::Odometry: `/state_estimation`
+  - tf tree
---
+++ sentry_decision/src/random_deci.h
@@ -0,0 +1,19 @@
+#ifndef RANDOM_DECI_H
+#define RANDOM_DECI_H
+
+float min_trigger_velo = 1.3;
+
+float init_goal_x = 4;
+float init_goal_y = 2;
+
+float max_x_range = 8;
+float min_x_range = 0;
+float max_y_range = 5;
+float min_y_range = 0;
+
+float trigger_distance = 0.8;
+
+double distance;
+bool publish_flag = true;
+
+#endif
